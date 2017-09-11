723ac9698529ec71fc609b7db376fe491bc29ad7
Eduardo Gonzalez <egonzalez@palantirsolutions.com>
2017-09-11 14:57:40 +0100

    Program finished message.

--- TrainsJavaEG/src/main/java/trains/Main.java
+++ TrainsJavaEG/src/main/java/trains/Main.java
@@ -30,7 +30,7 @@ public class Main {
 			System.out.println(graph.toString());
 		}		
 		
-		System.out.println("******** PROGRAM HAS FINISHED ********");
+		System.out.println("******** PROGRAM FINISHED ********");
 		
 	}
 	
